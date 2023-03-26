c4ff70b4fb0d9f6a31a188077bdf1fc7a7749e76
Junwei Li <junweili@JunweideMacBook-Air-3.local>
2023-03-26 17:34:03 -0400

    3-Connect to DB and complete sturture

---
+++ server.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const {logger} = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions.js')
+const PORT = process.env.PORT || 3500;
+const connectDB = require('./config/dbConn');
+const mongoose = require('mongoose');
+
+
+console.log(process.env.NODE_ENV);
+
+connectDB();
+
+app.use(express.json());
+
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+//routes
+app.use('/', require('./routes/root'));
+
+
+// app.all('*', (req, res) => {
+//     res.status(404)
+//     if (req.accepts('html')) {
+//         res.sendFile(path.join(__dirname, 'views', '404.html'))
+//     } else if (req.accepts('json')) {
+//         res.json({ message: '404 Not Found' })
+//     } else {
+//         res.type('txt').send('404 Not Found')
+//     }
+// })
+
+app.use((req, res) => {
+        res.status(404)
+        if (req.accepts('html')) {
+            res.sendFile(path.join(__dirname, 'views', '404.html'))
+        } else if (req.accepts('json')) {
+            res.json({ message: '404 Not Found' })
+        } else {
+            res.type('txt').send('404 Not Found')
+        }
+    });
+
+app.use(errorHandler);
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
